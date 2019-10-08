48d337a17e9d9acb8395d45700dc7ff90781ec68
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-08 13:50:33 -0700

    Move dynamic CSS changes from JS to CSS file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   $("textarea[name='text']").keyup(function(e) {
     const counter = $(this).parent().children().children('.counter');
+
     $(counter).html(() => {
       return 140 - $(this).val().length;
     })
 
     if ($(this).val().length > 140) {
-      $(counter).css({"color": "red"});
+      $(counter).addClass("tooManyCharacters");
     } else {
-      $(counter).css({"color": " #555149"})
+      $(counter).removeClass("tooManyCharacters");
     }
   });
 });
