2b33217c09d8f40414f399569a97fa9392f500ba
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-08 18:15:48 -0700

    Implement rederTweets function that appends an array of tweets to the page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,21 +4,37 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 // function that takes in a tweet object and returning a tweet
 // <article> element containing the entire HTML structure of the tweet
-const createTweetElement = function(tweet) {
+const createTweetElement = function (tweet) {
   const $result = `<article>
   <header>
     <div class="user">
@@ -44,10 +60,19 @@ const createTweetElement = function(tweet) {
   return $result;
 }
 
-const tweet = createTweetElement(tweetData);
+// loops through array of tweet objects
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+const renderTweets = function (tweets) {
+  for (const tweet of tweets) {
+    let result = createTweetElement(tweet);
+    $('#tweets-container').append(result);
+  }
+}
 
-$(document).ready(function() {
-  console.log(tweet); 
-  $('#tweets-container').append(tweet);
+$(document).ready(function () {
+  $("body").click(() => {
+    renderTweets(data);
+  })
 })
 
